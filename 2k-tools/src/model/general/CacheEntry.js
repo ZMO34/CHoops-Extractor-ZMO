afc96d5575bb36049ae8fc9033479566f6bd757e
ZMO34 <168148315+ZMO34@users.noreply.github.com>
2026-05-22 18:03:47 -0400

    Normalize cache entry archive fields as unsigned values

--- 2k-tools/src/model/general/CacheEntry.js
+++ 2k-tools/src/model/general/CacheEntry.js
@@ -1,3 +1,11 @@
+function normalizeUnsigned32(value) {
+    if (value === undefined || value === null) {
+        return 0;
+    }
+
+    return Number(BigInt(value) & 0xffffffffn);
+}
+
 class CacheEntry {
     constructor() {
         this.id = 0;
@@ -7,6 +15,38 @@ class CacheEntry {
         this.location = null;
         this.isSplit = false;
         this.splitSecondFileSize = 0;
+    };
+
+    set size(value) {
+        this._size = normalizeUnsigned32(value);
+    };
+
+    get size() {
+        return this._size || 0;
+    };
+
+    set offset(value) {
+        this._offset = normalizeUnsigned32(value);
+    };
+
+    get offset() {
+        return this._offset || 0;
+    };
+
+    set rawOffset(value) {
+        this._rawOffset = normalizeUnsigned32(value);
+    };
+
+    get rawOffset() {
+        return this._rawOffset || 0;
+    };
+
+    set splitSecondFileSize(value) {
+        this._splitSecondFileSize = normalizeUnsigned32(value);
+    };
+
+    get splitSecondFileSize() {
+        return this._splitSecondFileSize || 0;
     };
 };
 
