bfd4bc6ad637b1c8c08dfbbc1433965309720145
ZMO34 <168148315+ZMO34@users.noreply.github.com>
2026-05-25 14:34:58 -0400

    Expand PS3 IFF type coverage

--- 2k-tools/src/model/general/iff/IFFType.js
+++ 2k-tools/src/model/general/iff/IFFType.js
@@ -12,75 +12,71 @@ module.exports.TYPES = {
     AMBO: 10,
     HILT: 11,
     NAME: 12,
-    CDAN: 13
+    CDAN: 13,
+    FGCT: 14,
+    FXTWE: 15,
+    FRFG: 16,
+    SPCI: 17,
+    SHAP: 18,
+    STRG: 19,
+    SKEL: 20,
+    ROST: 21,
+    HTAJ: 22,
+    BLRB: 23,
+    UNLK: 24,
+    SINGL: 25,
+    AUSB: 26,
+    AOSS: 27,
+    AMCR: 28,
+    SCOS: 29,
+    UNAD: 30
 };
 
+const TYPE_STRING_MAP = {
+    TXTR: module.exports.TYPES.TXTR,
+    SCNE: module.exports.TYPES.SCNE,
+    AUDO: module.exports.TYPES.AUDO,
+    LAYT: module.exports.TYPES.LAYT,
+    MRKS: module.exports.TYPES.MRKS,
+    PRIV: module.exports.TYPES.PRIV,
+    TXT: module.exports.TYPES.TXT,
+    DRCT: module.exports.TYPES.DRCT,
+    CLTH: module.exports.TYPES.CLTH,
+    Clth: module.exports.TYPES.CLTH,
+    AMBO: module.exports.TYPES.AMBO,
+    HILT: module.exports.TYPES.HILT,
+    NAME: module.exports.TYPES.NAME,
+    CDAN: module.exports.TYPES.CDAN,
+    FGCT: module.exports.TYPES.FGCT,
+    FxTwe: module.exports.TYPES.FXTWE,
+    FRFG: module.exports.TYPES.FRFG,
+    SPCI: module.exports.TYPES.SPCI,
+    SHAP: module.exports.TYPES.SHAP,
+    STRG: module.exports.TYPES.STRG,
+    SKEL: module.exports.TYPES.SKEL,
+    ROST: module.exports.TYPES.ROST,
+    HTAJ: module.exports.TYPES.HTAJ,
+    BLRB: module.exports.TYPES.BLRB,
+    UNLK: module.exports.TYPES.UNLK,
+    Singl: module.exports.TYPES.SINGL,
+    AUSB: module.exports.TYPES.AUSB,
+    AOSS: module.exports.TYPES.AOSS,
+    AMCR: module.exports.TYPES.AMCR,
+    SCOS: module.exports.TYPES.SCOS,
+    UnAD: module.exports.TYPES.UNAD
+};
+
+const TYPE_VALUE_MAP = Object.entries(TYPE_STRING_MAP).reduce((map, [key, value]) => {
+    if (!map[value]) map[value] = key;
+    return map;
+}, {});
+
 module.exports.stringToType = (str) => {
-    if (str.slice(-1) === '\0') {
-        str = str.slice(0, str.length - 1);
-    }
-    
-    switch (str) {
-        case 'TXTR':
-            return this.TYPES.TXTR;
-        case 'SCNE':
-            return this.TYPES.SCNE;
-        case 'AUDO':
-            return this.TYPES.AUDO;
-        case 'LAYT':
-            return this.TYPES.LAYT;
-        case 'MRKS':
-            return this.TYPES.MRKS;
-        case 'PRIV':
-            return this.TYPES.PRIV;
-        case 'TXT':
-            return this.TYPES.TXT;
-        case 'DRCT':
-            return this.TYPES.DRCT;
-        case 'CLTH':
-            return this.TYPES.CLTH;
-        case 'AMBO':
-            return this.TYPES.AMBO;
-        case 'HILT':
-            return this.TYPES.HILT;
-        case 'NAME':
-            return this.TYPES.NAME;
-        case 'CDAN':
-            return this.TYPES.CDAN;
-        default:
-            return this.TYPES.UNKNOWN;
-    }
+    if (!str) return module.exports.TYPES.UNKNOWN;
+    if (str.slice(-1) === '\0') str = str.slice(0, str.length - 1);
+    return TYPE_STRING_MAP[str] || TYPE_STRING_MAP[str.toUpperCase()] || module.exports.TYPES.UNKNOWN;
 };
 
 module.exports.typeToString = (type) => {
-    switch (type) {
-        case this.TYPES.TXTR:
-            return 'TXTR';
-        case this.TYPES.SCNE:
-            return 'SCNE';
-        case this.TYPES.AUDO:
-            return 'AUDO';
-        case this.TYPES.LAYT:
-            return 'LAYT';
-        case this.TYPES.MRKS:
-            return 'MRKS';
-        case this.TYPES.PRIV:
-            return 'PRIV';
-        case this.TYPES.TXT:
-            return 'TXT';
-        case this.TYPES.DRCT:
-            return 'DRCT';
-        case this.TYPES.CLTH:
-            return 'CLTH';
-        case this.TYPES.AMBO:
-            return 'AMBO';
-        case this.TYPES.HILT:
-            return 'HILT';
-        case this.TYPES.NAME:
-            return 'NAME';
-        case this.TYPES.CDAN:
-            return 'CDAN';
-        default:
-            return 'UNKNOWN';
-    }
+    return TYPE_VALUE_MAP[type] || 'UNKNOWN';
 };
